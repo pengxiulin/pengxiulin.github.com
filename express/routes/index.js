9d992923643e3d10261b652f37e25b88befc83c6
pengxiulin <pengxiulin@live.com>
2013-06-23 23:26:15 +0800

    add initial page

--- express/routes/index.js
+++ express/routes/index.js
@@ -4,5 +4,16 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+      title: 'Express',
+      items:[
+          {
+              id:1,
+              title:"Initial Post",
+              time:{year:2013,month:4,day:9},
+              parent:0,
+              content:"<p>While HTML5 has a bunch of semantic elements, including new ones like &lt;article&gt; and &lt;nav&gt;, sometimes there just isn’t an element with the right meaning. What we want are ways to extend what we’ve got, to add extra semantics that are machine-readable — data that a browser, script, or robot can use.</p>"
+          }
+      ]
+  });
 };
